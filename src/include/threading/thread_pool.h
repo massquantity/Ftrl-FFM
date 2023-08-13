cdd5a6818f1513a41414e4eb07e262e79df34d13
massquantity <wdmjjxg@163.com>
2023-08-13 12:23:26 +0800

    chore: replace `std::result_of` with `std::invoke_result` as the former has been deprecated

--- src/include/threading/thread_pool.h
+++ src/include/threading/thread_pool.h
@@ -22,9 +22,9 @@ public:
   ThreadPool &operator=(const ThreadPool &) = delete;
   ThreadPool &operator=(ThreadPool &&) = delete;
 
-  template<typename F, typename... Args>
-  auto enqueue(F&& f, Args&&... args)  // NOLINT
-    -> std::future<typename std::result_of<F(Args...)>::type>;
+  template <typename F, typename... Args>
+  auto enqueue(F &&f, Args &&...args)
+      -> std::future<typename std::invoke_result<F, Args...>::type>;
 
   void synchronize(int wait_count);
   size_t get_num_threads();
@@ -64,15 +64,15 @@ inline ThreadPool::ThreadPool(size_t n_threads) {
   }
 }
 
-template<typename F, typename... Args>
-auto ThreadPool::enqueue(F&& f, Args&&... args) // NOLINT
-    -> std::future<typename std::result_of<F(Args...)>::type> {
+template <typename F, typename... Args>
+auto ThreadPool::enqueue(F &&f, Args &&...args)
+    -> std::future<typename std::invoke_result<F, Args...>::type> {
   if (stop) {
     throw std::runtime_error("can't enqueue on stopped ThreadPool...");
   }
-  using return_type = typename std::result_of<F(Args...)>::type;
-  std::function<decltype(f(args...))()> func = std::bind(
-      std::forward<F>(f), std::forward<Args>(args)...);
+  using return_type = typename std::invoke_result<F, Args...>::type;
+  std::function<decltype(f(args...))()> func =
+      std::bind(std::forward<F>(f), std::forward<Args>(args)...);
   auto task_ptr = std::make_shared<std::packaged_task<return_type()>>(func);
   // std::future<return_type> res = task->get_future();
   std::unique_lock<std::mutex> lock(queue_mutex);
